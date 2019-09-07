5df44225d3588c3bc284da4f50f23c43a3eff1b2
pathum <awpathum@gmail.com>
2019-09-08 02:15:39 +0530

    appfam end point added for flutter app

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+
 app.post('/', function (req, res) {
 
     var userlogindetails = req.body;
@@ -172,7 +173,7 @@ app.post('/', (req, res) => {
     console.log("Completed");
 });
 
-app.get('/', (req, res) => {
+app.get('/appfam', (req, res) => {
     ApprovedFamily.find((err, doc) => {
         res.send(doc)
     })
