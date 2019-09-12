8fd7a33b5ca03e1f115b664135ea2325bf3df74e
Sahan <shnrndk@gmail.com>
2019-09-12 10:55:48 +0530

    changes added

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,13 @@
 var express = require('express');
 var app = express();
 var connection = require('./connection');
+
 var Notification = require('./Schemas/NotificationSchema');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 
 
 app.use(require('./routes'));
