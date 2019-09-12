2887df33a63c20d8a40563e6fa2a8a14360a0b27
SupuniNuwarapaksha <supuninuwarapaksha@gmail.com>
2019-09-12 06:06:12 +0530

    clinic scheduling

---
+++ Backend/Schemas/clinic.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var clinicScheduling= new Schema({
+    "clinic_name":String,
+    "clinic_type": String,
+    "date": Date
+},{collection: 'clinicScheduling'});
+var  clinicScheduling= mongoose.model('clinicScheduling', clinicScheduling);
+
+module.exports = clinicScheduling;
