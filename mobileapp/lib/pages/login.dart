6b8b81f786133b9c55f6dd5f3ce6d2a1735771ca
pathum <awpathum@gmail.com>
2019-09-13 16:45:12 +0530

    login page design done

--- mobileapp/lib/pages/login.dart
+++ mobileapp/lib/pages/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+//import 'dart:io';
+//import 'package:flutter/services.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -6,17 +8,19 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  String email;
+  final _formKey = GlobalKey<FormState>();
+  var email;
+  var password;
+
   @override
   Widget build(BuildContext context) {
+    final motherLogo = const ImageIcon(AssetImage("images/mother.png"),
+        size: 200.0, color: Colors.black);
+
     final emailField = TextFormField(
-      
       validator: (input) {
         if (input.isEmpty) {
           return 'Please Enter Index Number';
-        }
-        if (input.substring(0, 2) != '20') {
-          return 'Index number not valid';
         }
       },
       obscureText: false,
@@ -28,8 +32,78 @@ class _LoginState extends State<Login> {
               OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
       onSaved: (input) => email = input,
     );
+
+    final pwField = TextFormField(
+      validator: (input) {
+        if (input.isEmpty) {
+          return 'Please Enter Password';
+        }
+      },
+      obscureText: true,
+      //style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+      onSaved: (input) => password = input,
+    );
+
+    final loginButton = Container(
+      width: 315.0,
+      height: 48.0,
+      child: new RaisedButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(10.0)),
+        child: new Text('Login'),
+        onPressed: () {},
+      ),
+    );
+    final forgotPasswordText = FlatButton(
+      child: Text('Forgot Password?'),
+      padding: EdgeInsets.all(1.0),
+      textColor: Colors.grey,
+      //splashColor: Colors.blueAccent,
+      onPressed: () {},
+    );
     return Scaffold(
-      
-      );
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 100, 0, 20),
+                child: motherLogo,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      emailField,
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      pwField,
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      loginButton,
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      forgotPasswordText,
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
