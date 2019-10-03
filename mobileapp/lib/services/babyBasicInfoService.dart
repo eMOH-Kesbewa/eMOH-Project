75a20dff7bd572ac53f8f3b2663e633861bcfb83
pathum <awpathum@gmail.com>
2019-10-03 10:20:41 +0530

    babyBasic information page done

--- mobileapp/lib/services/babyBasicInfoService.dart
+++ mobileapp/lib/services/babyBasicInfoService.dart
@@ -5,41 +5,70 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'globals.dart' as globals;
 
-Future<Family> fetchFamily() async {
-  print('fetch family function');
-  final response =
-      await http.get('https://protected-bayou-52277.herokuapp.com/babies/viewbyid/${globals.globalEmail}');
-      print('status code');
-      print(response.statusCode);
-      print(response.body);
+Future<Baby> fetchBaby() async {
+  print('fetch Baby function');
+  //String _babyId = globals.BabyId + babyIndex;
+  final response = await http.get(
+      'https://protected-bayou-52277.herokuapp.com/babies/viewbyid/${globals.babyId}');
+  print('status code');
+  print(response.statusCode);
+  print(response.body);
   // final json = jsonDecode(response.body);
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON.
-  //return (json.decode(response.body));
-    //print(Family.fromJson(json.decode(response.body)));
-    return Family.fromJson(json.decode(response.body)[0]);
-  } else {  
+    //return (json.decode(response.body));
+    //print(Baby.fromJson(json.decode(response.body)));
+    return Baby.fromJson(json.decode(response.body)[0]);
+  } else {
     // If that call was not successful, throw an error.
-    throw Exception('Failed to load Family');
+    throw Exception('Failed to load Baby');
   }
 }
 
-class Family {
-  final String idNumber;
-  final String vilID;
-  final String wifeName;
-  final String husbandName;
-  final String childrenCount;
+class Baby {
+  final String name;
+  final String id;
+  final String birthday;
+  final String regDate;
+  final String nameOfMother;
+  final String ageOfMother;
+  final String address;
+  //final String childrenCount;
+  final String phmArea;
+  final String mohArea;
 
-  Family({this.idNumber, this.vilID, this.wifeName, this.husbandName,this.childrenCount});
+  Baby(
+      {this.id,
+      this.mohArea,
+      this.phmArea,
+      this.name,
+      this.address,
+      this.ageOfMother,
+      this.birthday,
+      //this.childrenCount,
+      this.nameOfMother,
+      this.regDate});
 
-  factory Family.fromJson(Map<String, dynamic> json) {
-    return Family(
-      idNumber: json['Identity_number'],
-      vilID: json['village_id'],
-      wifeName: json['Name_of_wife'],
-      husbandName: json['Name_of_husband'],
-      childrenCount: json['Number_of_living_childern']
+  factory Baby.fromJson(Map<String, dynamic> json) {
+    
+    return Baby(
+      // // idNumber: json['Identity_number'],
+      // vilID: json['village_id'],
+      // wifeName: json['Name_of_wife'],
+      // husbandName: json['Name_of_husband'],
+      // childrenCount: json['Number_of_living_childern']
+
+      id: json["baby_id"],
+      phmArea: json["PHM_area"],
+      mohArea: json["MOH_area"],
+      name: json["name_of_child"],
+      birthday: json["date_of_birth_of_child"],
+      regDate: json["date_of_registered"],
+      nameOfMother: json["name_of_mother"],
+      ageOfMother: json["age_of_mother"].toString(),
+      address: json["address"],
+      // childrenCount:
+      //     json["total_Number_of_children_alive_including_this_child"].toString(),
     );
   }
 }
--- mobileapp/lib/services/globals.dart
+++ mobileapp/lib/services/globals.dart
@@ -2,3 +2,5 @@ library mobileapp.globals;
 
 String globalEmail = 'xyz';
 int selectedIndex = 0;
+String familyId = 'fam123';
+String babyId;
