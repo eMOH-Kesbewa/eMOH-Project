c3560fdabfd0f08bc3cd51d730a06be97c9043e1
pathum <awpathum@gmail.com>
2019-10-05 08:25:10 +0530

    special reasons is not working

--- mobileapp/lib/services/babyService/babyProtectionServeice.dart
+++ mobileapp/lib/services/babyService/babyProtectionServeice.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobileapp/services/style.dart';
 import '../globals.dart' as globals;
 
 Future<Baby> fetchBaby() async {
@@ -40,6 +41,17 @@ class Baby {
   final String breastFeedingUnstability;
   final String breastFeedingConnection;
   final String checkCongenitalHypothyroidism;
+  final String prematureBirthsStatus;
+  final String lowBirthWeightStatus;
+  final String neonatalAbnomalitiesStatus;
+  final String inheritedProblemsStatus;
+  final String congenitalHypothyroidismState;
+  final String serverIllnessOfTheMotherAfterDeliveryStatus;
+  final String breastfeedingAtFirstSixMonthsStatus;
+  final String impairmentsOfGrowthStatus;
+  final String deathOfMotherOrFatherStatus;
+  final String separationOrDepatureOfMotherOrFatherStatus;
+  final String otherStatus;
 
   Baby({
     this.deliveryMethod,
@@ -55,9 +67,50 @@ class Baby {
     this.breastFeedingUnstability,
     this.breastFeedingConnection,
     this.checkCongenitalHypothyroidism,
+    this.prematureBirthsStatus,
+    this.lowBirthWeightStatus,
+    this.neonatalAbnomalitiesStatus,
+    this.inheritedProblemsStatus,
+    this.congenitalHypothyroidismState,
+    this.serverIllnessOfTheMotherAfterDeliveryStatus,
+    this.breastfeedingAtFirstSixMonthsStatus,
+    this.impairmentsOfGrowthStatus,
+    this.deathOfMotherOrFatherStatus,
+    this.separationOrDepatureOfMotherOrFatherStatus,
+    this.otherStatus,
   });
 
+ List<Widget> getReasons() {
+      // <<<<< Note this change for the return type
+      List reasons = new List<Widget>();
+      if (prematureBirthsStatus != "Normal") {
+        reasons.add(
+          new ListTile(
+            leading: warningIcon,
+            title: Text(prematureBirthsStatus),
+          ),
+        );
+      }
+      // int i = 0;
+      // for (i = 0; i < 5; i++) {
+      //   reasons.add(
+      //     new RadioListTile<String>(
+      //       title: const Text('Lafayette'),
+      //       value: "c",
+      //       groupValue: "x",
+      //       onChanged: (_) {},
+      //     ),
+      //   );
+      // }
+      return reasons;
+    }
+  
+
   factory Baby.fromJson(Map<String, dynamic> json) {
+    List _reasons = new List();
+
+    
+
     return Baby(
       // // idNumber: json['Identity_number'],
       // vilID: json['village_id'],
@@ -81,8 +134,26 @@ class Baby {
       checkCongenitalHypothyroidism:
           json["does_check_congenital_hypothyroidism"],
 
+      prematureBirthsStatus: json["premature_births_status"],
+      lowBirthWeightStatus: json["low_birth_weight_statu"],
+      neonatalAbnomalitiesStatus: json["neonatal_abnomalities_status"],
+      inheritedProblemsStatus: json["inherited_problems_status"],
+      congenitalHypothyroidismState: json["congenital_hypothyroidism_state"],
+      serverIllnessOfTheMotherAfterDeliveryStatus:
+          json["server_illness_of_the_mother_after_delivery_status"],
+      breastfeedingAtFirstSixMonthsStatus:
+          json["breastfeeding_at_first_six_months_status"],
+      impairmentsOfGrowthStatus: json["impairments_of_growth_status"],
+      deathOfMotherOrFatherStatus: json["death_of_mother_or_father_status"],
+      separationOrDepatureOfMotherOrFatherStatus:
+          json["separation_or_depature_of_mother_or_father_status"],
+      otherStatus: json["other_status"],
+
       // childrenCount:
       //     json["total_Number_of_children_alive_including_this_child"].toString(),
     );
   }
+
+ 
+  
 }
