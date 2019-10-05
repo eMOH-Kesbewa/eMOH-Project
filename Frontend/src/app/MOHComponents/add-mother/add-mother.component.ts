2885a384bf0f9c677928aeed878601e2a7a9b7fb
Sahan <shnrndk@gmail.com>
2019-10-05 12:16:57 +0530

    Add mother component added

---
+++ Frontend/src/app/MOHComponents/add-mother/add-mother.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-mother',
+  templateUrl: './add-mother.component.html',
+  styleUrls: ['./add-mother.component.scss']
+})
+export class AddMotherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
