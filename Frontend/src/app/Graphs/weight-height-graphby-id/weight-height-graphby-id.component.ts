fb9ee6bdfb8465df4d5ed25b3c098806e8a5d360
Sahan <shnrndk@gmail.com>
2019-10-14 18:39:30 +0530

    Weight Height Graph Component by id Added

---
+++ Frontend/src/app/Graphs/weight-height-graphby-id/weight-height-graphby-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weight-height-graphby-id',
+  templateUrl: './weight-height-graphby-id.component.html',
+  styleUrls: ['./weight-height-graphby-id.component.scss']
+})
+export class WeightHeightGraphbyIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
