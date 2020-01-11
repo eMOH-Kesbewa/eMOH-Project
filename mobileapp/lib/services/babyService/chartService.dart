4c2c0682761add8179447ff77abda1ad28890654
awpathum <awpathum@gmail.com>
2020-01-11 20:07:22 +0530

    chart is implemnting

--- mobileapp/lib/services/babyService/chartService.dart
+++ mobileapp/lib/services/babyService/chartService.dart
@@ -1,75 +1,75 @@
-// import 'dart:async';
-// import 'dart:convert';
+// // import 'dart:async';
+// // import 'dart:convert';
 
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import '../globals.dart' as globals;
+// // import 'package:flutter/material.dart';
+// // import 'package:http/http.dart' as http;
+// // import '../globals.dart' as globals;
 
-// Future<Baby> fetchBaby() async {
-//   print('fetch Baby function');
-//   //String _babyId = globals.BabyId + babyIndex;
-//   final response = await http.get(
-//       'https://protected-bayou-52277.herokuapp.com/babies/viewbyid/${globals.babyId}');
+// // Future<Baby> fetchBaby() async {
+// //   print('fetch Baby function');
+// //   //String _babyId = globals.BabyId + babyIndex;
+// //   final response = await http.get(
+// //       'https://protected-bayou-52277.herokuapp.com/babies/viewbyid/${globals.babyId}');
 
-//   // final json = jsonDecode(response.body);
-//   if (response.statusCode == 200) {
-//     // If the call to the server was successful, parse the JSON.
-//     //return (json.decode(response.body));
-//     //print(Baby.fromJson(json.decode(response.body)));
-//     return Baby.fromJson(json.decode(response.body)[0]);
-//   } else {
-//     // If that call was not successful, throw an error.
-//     throw Exception('Failed to load Baby');
-//   }
-// }
+// //   // final json = jsonDecode(response.body);
+// //   if (response.statusCode == 200) {
+// //     // If the call to the server was successful, parse the JSON.
+// //     //return (json.decode(response.body));
+// //     //print(Baby.fromJson(json.decode(response.body)));
+// //     return Baby.fromJson(json.decode(response.body)[0]);
+// //   } else {
+// //     // If that call was not successful, throw an error.
+// //     throw Exception('Failed to load Baby');
+// //   }
+// // }
 
-// class Baby {
-//     x1;
-//     y1;
-//     x2;
-//     y2;
-//     x3;
-//     y3;
-//     x4;
-//     y4;
-//     x5;
-//     y5;
-//     x6;
-//     y6;
-//     x7;
-//     y7;
-//     x8;
-//     y8;
-//     x9;
-//     y9;
-//     x10;
-//     y10;
+// // class Baby {
+// //     x1;
+// //     y1;
+// //     x2;
+// //     y2;
+// //     x3;
+// //     y3;
+// //     x4;
+// //     y4;
+// //     x5;
+// //     y5;
+// //     x6;
+// //     y6;
+// //     x7;
+// //     y7;
+// //     x8;
+// //     y8;
+// //     x9;
+// //     y9;
+// //     x10;
+// //     y10;
 
-//   Baby(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3, this.x4, this.y4, this.x5, this.y5, this.x6, this.y6, this.x7, this.y7, this.x8, this.y8, this.x9, this.y9, this.x10, this.y10
-//      });
+// //   Baby(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3, this.x4, this.y4, this.x5, this.y5, this.x6, this.y6, this.x7, this.y7, this.x8, this.y8, this.x9, this.y9, this.x10, this.y10
+// //      });
 
-//   factory Baby.fromJson(Map<String, dynamic> json) {
-//     return Baby(
-//       // // idNumber: json['Identity_number'],
-//       // vilID: json['village_id'],
-//       // wifeName: json['Name_of_wife'],
-//       // husbandName: json['Name_of_husband'],
-//       // childrenCount: json['Number_of_living_childern']
+// //   factory Baby.fromJson(Map<String, dynamic> json) {
+// //     return Baby(
+// //       // // idNumber: json['Identity_number'],
+// //       // vilID: json['village_id'],
+// //       // wifeName: json['Name_of_wife'],
+// //       // husbandName: json['Name_of_husband'],
+// //       // childrenCount: json['Number_of_living_childern']
 
-//       id: json["baby_id"],
-//       phmArea: json["PHM_area"],
-//       mohArea: json["MOH_area"],
-//       name: json["name_of_child"],
-//       birthday: json["date_of_birth_of_child"],
-//       regDate: json["date_of_registered"],
-//       nameOfMother: json["name_of_mother"],
-//       ageOfMother: json["age_of_mother"].toString(),
-//       address: json["address"],
-//       // childrenCount:
-//       //     json["total_Number_of_children_alive_including_this_child"].toString(),
-//     );
-//   }
-// }
+// //       id: json["baby_id"],
+// //       phmArea: json["PHM_area"],
+// //       mohArea: json["MOH_area"],
+// //       name: json["name_of_child"],
+// //       birthday: json["date_of_birth_of_child"],
+// //       regDate: json["date_of_registered"],
+// //       nameOfMother: json["name_of_mother"],
+// //       ageOfMother: json["age_of_mother"].toString(),
+// //       address: json["address"],
+// //       // childrenCount:
+// //       //     json["total_Number_of_children_alive_including_this_child"].toString(),
+// //     );
+// //   }
+// // }
 
 import 'dart:async';
 import 'dart:convert';
@@ -78,10 +78,11 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
 import '../globals.dart' as globals;
+import '../chartdata.dart' as chart;
 
 var logger = Logger();
 
-Future<Baby> fetchBaby() async {
+Future fetchChart() async {
   print('fetch Baby function');
   //String _babyId = globals.BabyId + babyIndex;
 
@@ -96,6 +97,8 @@ Future<Baby> fetchBaby() async {
     //return (json.decode(response.body));
     // logger.i(Baby.fromJson(json.decode(response.body)));
 //    print(Baby.fromJson(json.decode(response.body)));
+
+   // logger.wtf(json.decode(response.body)[0]);
     return Baby.fromJson(json.decode(response.body)[0]);
   } else {
     // If that call was not successful, throw an error.
@@ -148,7 +151,13 @@ class Baby {
       this.y10});
 
   factory Baby.fromJson(Map<String, dynamic> json) {
-    // logger.wtf(json['x1']);
+    logger.wtf(json['x1']);
+    // chart.x1 = json['x1'].toString();
+    // logger.d(chart.x1);
+    
+    chart.x1 = json['x1'].toString();
+    logger.e(json['x1']);
+    logger.d(chart.x1);
     return Baby(
       x1: json['x1'].toDouble(),
       y1: json['y1'].toDouble(),
@@ -175,3 +184,41 @@ class Baby {
     );
   }
 }
+
+// import 'dart:async';
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:logger/logger.dart';
+// import '../chartdata.dart' as chart;
+// import '../globals.dart' as globals;
+
+// var logger = Logger();
+
+// List<String> id = List();
+// List<String> name = List();
+// List<String> discription = List();
+// List<String> date = List();
+
+// Future fetchChart() async {
+//  // var x = await globals.getGlobals();
+//   final response = await http
+//       .get('https://protected-bayou-52277.herokuapp.com/babies/viewbyid/${globals.babyId}');
+
+//   if (response.statusCode == 200) {
+//     logger.wtf(response.statusCode);
+//     logger.e('hello');
+//     var result = json.decode(response.body[0]);
+//     logger.v(response.body);
+//     // logger.i(result);
+//     chart.x1 = result.x1;
+//     // logger.v(result.x1);
+//     // logger.d(chart.x1);
+//     return result;
+
+//     //return [id,name,]
+//   } else {
+//     // If that call was not successful, throw an error.
+//     throw Exception('Failed to load Family');
+//   }
+// }
