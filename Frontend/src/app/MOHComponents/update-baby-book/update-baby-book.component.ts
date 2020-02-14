1e4a610b42386124b7b285fb16213d0878d8f750
shnrndk <shnrndk@gmail.com>
2020-02-14 21:36:46 +0530

    Update  baby Book AutoFill Feature Added

--- Frontend/src/app/MOHComponents/update-baby-book/update-baby-book.component.ts
+++ Frontend/src/app/MOHComponents/update-baby-book/update-baby-book.component.ts
@@ -8,6 +8,8 @@ import { from } from 'rxjs';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
 import {  minDate,prop, } from "@rxweb/reactive-form-validators";
+import { MotherbabyjoinedService } from 'app/Services/motherbabyjoined.service';
+import { MotherService } from 'app/Services/mother.service';
 
 @Component({
   selector: 'app-update-baby-book',
@@ -30,7 +32,7 @@ export class UpdateBabyBookComponent implements OnInit {
 	date_of_registered: string;
 
 
-  constructor(private router: Router,private formBuilder: FormBuilder, private _snackBar: MatSnackBar,private addbabyService: BabiesService, private ngZone: NgZone,private activeroute: ActivatedRoute) { }
+  constructor(private motherService: MotherService,private router: Router,private formBuilder: FormBuilder, private _snackBar: MatSnackBar,private addbabyService: BabiesService, private ngZone: NgZone,private activeroute: ActivatedRoute,private motherbabyjoinedservice : MotherbabyjoinedService) { }
 
   autoRenew : FormControl;
   public babyID;
@@ -713,20 +715,20 @@ export class UpdateBabyBookComponent implements OnInit {
       });
 
       this.addbabyService.getbabydata(this.babyID).subscribe(data => {
-        this.DOB =  this.dateconverter(data[0] ['date_of_birth_of_child'])
+      //  this.DOB =  this.dateconverter(data[0] ['date_of_birth_of_child'])
         this.BabyForm.patchValue({
           
-          baby_id: data[0] ['baby_id'],
-      name_of_child: data[0] ['name_of_child'],
-      date_of_birth_of_child: this.dateconverter(data[0] ['date_of_birth_of_child']),
-      date_of_registered: this.dateconverter( data[0] ['date_of_registered']),
-      name_of_mother: data[0] ['name_of_mother'],
-      age_of_mother: data[0] ['age_of_mother'],
-      address: data[0] ['address'],
+         // baby_id: data[0] ['baby_id'],
+     // name_of_child: data[0] ['name_of_child'],
+     // date_of_birth_of_child: this.dateconverter(data[0] ['date_of_birth_of_child']),
+     // date_of_registered: this.dateconverter( data[0] ['date_of_registered']),
+    //  name_of_mother: data[0] ['name_of_mother'],
+      //age_of_mother: data[0] ['age_of_mother'],
+      //address: data[0] ['address'],
       Number_of_apgas_1m: data[0] ['Number_of_apgas_1m'],
       Number_of_apgas_5m: data[0] ['Number_of_apgas_5m'],
       Number_of_apgas_10m: data[0] ['Number_of_apgas_10m'],
-      total_Number_of_children_alive_including_this_child: data[0] ['total_Number_of_children_alive_including_this_child'],
+    //  total_Number_of_children_alive_including_this_child: data[0] ['total_Number_of_children_alive_including_this_child'],
       method_of_delivery:data[0] ['method_of_delivery'],
       birth_weight: data[0] ['birth_weight'],
       gridle_circumference_at_birth: data[0] ['gridle_circumference_at_birth'],
@@ -1389,6 +1391,29 @@ export class UpdateBabyBookComponent implements OnInit {
         });
     });
 
+ 
+
+    this.motherbabyjoinedservice.getmotherbabyjoineddataById(this.babyID).subscribe(data => {
+      this.BabyForm.patchValue({
+        baby_id: data[0]['baby_id'],
+        name_of_child: data[0]['child_name'],
+        date_of_birth_of_child: this.dateconverter(data[0]['date_of_birth']),
+        date_of_registered: this.dateconverter( data[0]['registration_date']),
+        name_of_mother: data[0]['mothers_name'],
+        age_of_mother: data[0]['age_of_mother'],
+        address: data[0]['address']
+      })
+    }
+    )
+    this.motherService.getmothersdataById(this.getMotherId(this.babyID)).subscribe(data => {
+      this.BabyForm.patchValue({
+        
+        age_of_mother: data[0]['age'],
+        total_Number_of_children_alive_including_this_child: parseInt(data[0]['no_of_living_children'])+1
+      })
+    }
+
+    )
     this.autoRenew = new FormControl();
     this.onChange();
     
@@ -1464,6 +1489,10 @@ export class UpdateBabyBookComponent implements OnInit {
             return dateBirth[0]+"-"+newMonth+"-"+dateBirth[2]
           }
 
+          getMotherId(babyId){
+            let mother_id = babyId.slice(0, 6);
+            return mother_id;
+          }
         
  
 }
