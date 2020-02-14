3cf1ddb059fb00fb9b45f4fe9a866b38547c489d
shnrndk <shnrndk@gmail.com>
2020-02-14 20:17:42 +0530

    Display low weight babies  added to Sp.Attention

---
+++ Frontend/src/app/Services/special-attention.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { lowweightDetails } from './Models/lowweightDetails';
+import { Weight } from './Models/weight';
+@Injectable({
+  providedIn: 'root'
+})
+export class SpecialAttentionService {
+
+  constructor(private http: HttpClient) { }
+
+  getLowWeightBabies(months){          
+    console.log(months)   
+    let uri = `http://localhost:3000/specialAttention/lowWeightFirstYear/${months}`
+    return this.http.get<Weight>(uri)
+   }
+  
+}
